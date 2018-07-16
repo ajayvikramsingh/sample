5406f608dcf0dc193c9a71ffe79439467bf9fa81
ajayvikramsingh <ajayvik123ram@gmail.com>
2018-07-16 16:52:32 +0530

    add item

--- src/App.js
+++ src/App.js
@@ -3,17 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      newToDoItem: '',
+      toDoList: ['a', 's']
+    };
+    this.onBtnClick = this.onBtnClick.bind(this);
+    this.onNewTodoItemChange = this.onNewTodoItemChange.bind(this);
+  }
+
+  onNewTodoItemChange(event){
+    let newState = JSON.parse(JSON.stringify(this.state));
+    newState.newToDoItem = event.target.value;
+    this.setState(newState);
+  }
+
+  onBtnClick(){
+    let newState =JSON.parse(JSON.stringify (this.state));
+    newState.toDoList.push(newState.newToDoItem);
+    newState.newToDoItem = '';
+    this.setState(newState);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>To Do</h1>
+        <input type="text" name="newToDoItem" value={this.state.newToDoItem} onChange={this.onNewTodoItemChange} />
+        <button onClick={this.onBtnClick}>Add</button>        
+        <div>
+          <ul>
+            {this.state.toDoList.map((toDoItem, index) => {return <li>{toDoItem}</li>})}
+          </ul>
+        </div>
       </div>
+
+
     );
   }
 }
