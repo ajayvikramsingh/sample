65a244edd62fa7768140b0a6816ac939282d7092
ajayvikramsingh <ajayvik123ram@gmail.com>
2018-07-17 09:44:18 +0530

    edit operation implemented

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ class App extends Component {
     super(props);
     this.state = {
       newToDoItem: '',
-      toDoList: [],           
+      toDoList: [],
+      isEditMode: false,
+      editItemIndex: -1,
     };
-    this.onBtnClick = this.onBtnClick.bind(this);
+    this.onAddBtnClick = this.onAddBtnClick.bind(this);
     this.onNewTodoItemChange = this.onNewTodoItemChange.bind(this);
-    this.onRemoveclick=this.onRemoveclick.bind(this);
+    this.onRemoveClick=this.onRemoveClick.bind(this);
+    this.onEditClick=this.onEditClick.bind(this);
   }
 
   onNewTodoItemChange(event){
@@ -20,29 +23,47 @@ class App extends Component {
     this.setState(newState);
   }
 
-  onBtnClick(){
+  onAddBtnClick(){
     let newState =JSON.parse(JSON.stringify (this.state));
-    newState.toDoList.push(newState.newToDoItem);
+    if(this.state.isEditMode){
+      newState.toDoList.splice(newState.editItemIndex, 1, newState.newToDoItem);
+      newState.editItemIndex = -1,
+      newState.isEditMode = false;      
+    } else {
+      newState.toDoList.push(newState.newToDoItem);
+    }
     newState.newToDoItem = '';
     this.setState(newState);
   }
-  onRemoveclick(index)
+  onRemoveClick(index)
   {
     let newState=JSON.parse(JSON.stringify(this.state));
     newState.toDoList.splice(index, 1);
     this.setState(newState);
   }
+  onEditClick(index)
+  {
+    let newState=JSON.parse(JSON.stringify(this.state));
+    newState.newToDoItem=newState.toDoList[index];
+    newState.toDoList[index]=newState.newToDoItem;
+    newState.isEditMode = true;
+    newState.editItemIndex = index;
+    this.setState(newState);
+  }
 
-  render() {
+
+   render() {
     return (
 
 <div className="App">
         <h1 >To Do</h1>
         <input type="text" name="newToDoItem" value={this.state.newToDoItem} onChange={this.onNewTodoItemChange} />
-        <button onClick={this.onBtnClick}>Add</button>        
+        <button disabled={this.state.newToDoItem === ''} onClick={this.onAddBtnClick}>{this.state.isEditMode ? 'UPDATE' : 'ADD'}</button>        
         <div>
           <ol>
-            {this.state.toDoList.map((toDoItem, index) => {return <li key={index}>{toDoItem}<button onClick={()=>{this.onRemoveclick(index)}}>remove</button></li>} )}
+            {this.state.toDoList.map((toDoItem, index) => 
+              {return <li key={index}>{toDoItem}<button onClick={()=>{this.onRemoveClick(index)}}>remove</button>
+              <button onClick={()=>{this.onEditClick(index)}}>edit</button></li>})}
           </ol>
         </div>
       </div>
