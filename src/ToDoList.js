e02756acc4da6c1e3843a49aaa6ba1715ca541b0
ajayvikramsingh <ajayvik123ram@gmail.com>
2018-07-22 09:10:14 +0530

    schoolapp

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css';
 
 const toDoItemStyle = {
     textDecoration: 'line-through',
@@ -14,13 +15,33 @@ class toDoList extends Component {
                 <ol>
                     {this.props.toDoList.map((toDoItem, index) => {
                         return <li key={index}>
-                                    <div style={toDoItem.isDone ? toDoItemStyle : null}>{toDoItem.title}</div>
-                                    <button onClick={() => { this.props.onRemoveClick(index) }}>remove</button>                                    
-                                    <button onClick={() => { this.props.onEditClick(index) }}>edit</button>
-                                    <button onClick={() => this.props.onDoneClick(index)}>{toDoItem.isDone ? 'Undo' : 'Done'}</button>
-                                </li>
+                            <div style={toDoItem.isDone ? toDoItemStyle : null}>{toDoItem.title}</div>
+                            <button onClick={() => { this.props.onRemoveClick(index) }}>remove</button>
+                            <button onClick={() => { this.props.onEditClick(index) }}>edit</button>
+                            <button onClick={() => this.props.onDoneClick(index)}>{toDoItem.isDone ? 'Undo' : 'Done'}</button>
+                        </li>
                     })}
                 </ol>
+                <table>
+                    <tr>
+                        <th>Title</th>
+                        <th>Status</th>
+                        <th>Edit</th>
+                        <th>Remove</th>
+                    </tr>
+                    <tbody>
+                        {
+                            this.props.toDoList.map((toDoItem, index) => {
+                                return (<tr>
+                                    <td>{toDoItem.title}</td>
+                                    <td><button onClick={() => this.props.onDoneClick(index)}>{toDoItem.isDone ? 'Undo' : 'Done'}</button></td>
+                                    <td><button onClick={() => { this.props.onEditClick(index) }}>edit</button></td>
+                                    <td><button onClick={() => { this.props.onRemoveClick(index) }}>remove</button></td>
+                                </tr>);
+                            })
+                        }
+                    </tbody>
+                </table>
             </div>
         );
     }
